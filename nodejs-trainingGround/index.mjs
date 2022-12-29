8070b7782268ef9970253bb5d109193f6570a527
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2022-12-29 20:05:17 +0100

    conditionnal

---
+++ nodejs-trainingGround/index.mjs
@@ -0,0 +1,27 @@
+/**before */
+/*function getAge(birthYear, currentYear) {
+  return currentYear - birthYear;
+}
+module.exports.getAge = getAge;*/
+
+/*after optimized code */
+export const getAge = (birthYear , currentYear) => currentYear - birthYear;
+export const getAgeGroup = (Age) => {
+  if(Age < 4) {
+    return "toddler";
+  }
+  if(Age < 13) {
+    return "kid";
+  }
+  if(Age < 20) {
+    return "teenager";
+  }
+  if(Age < 40) {
+    return "adult";
+  }
+  if(Age > 39) {
+    return "old";
+  }
+}
+
+
---
+++ nodejs-trainingGround/tests.mjs
@@ -0,0 +1,58 @@
+/* eslint-disable no-undef */
+import  assert  from 'assert';
+import { getAge,getAgeGroup } from './index.mjs';
+
+describe('age calculator', () => {
+  it('someone born 1972 is 50 2022', () => {
+    // act
+    const result = getAge(1972, 2022);
+
+    // assert
+    assert.equal(result, 50);
+  });
+  it('someone born 2021 is 1 2022', () => {
+    // arrange
+    //act
+    const result = getAge(2021, 2022);
+
+    // assert
+    assert.equal(result, 1);
+  });
+});
+describe('age classifier', () => {
+  it('0-3 years old should be toddler', () => {
+    //arrange
+    //act
+    const result = getAgeGroup(0);
+    // assert
+    assert.equal(result, "toddler");
+  });
+  it('4-12 years old should be kid', () => {
+    //arrange
+    //act
+    const result = getAgeGroup(4);
+    // assert
+    assert.equal(result, "kid");
+  }); 
+  it('13-19 years old should be teenager', () => {
+    //arrange
+    //act
+    const result = getAgeGroup(13);
+    // assert
+    assert.equal(result, "teenager");
+  });
+  it('20-39 years old should be adult', () => {
+    //arrange
+    //act
+    const result = getAgeGroup(20);
+    // assert
+    assert.equal(result, "adult");
+  });
+  it('above 39 years old ... you are just old', () => {
+    //arrange
+    //act
+    const result = getAgeGroup(40);
+    // assert
+    assert.equal(result, "old");
+  });
+});
