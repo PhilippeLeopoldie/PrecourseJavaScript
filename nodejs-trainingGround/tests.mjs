fa4c31babea6d42d3575728581f1015c3858b514
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2022-12-30 17:45:40 +0100

    Variables-constant or not undefined or null

--- nodejs-trainingGround/tests.mjs
+++ nodejs-trainingGround/tests.mjs
@@ -69,4 +69,48 @@ describe('age classifier', () => {
     // assert
     assert.equal(result, "Age can't be negative");
   });
+  it('unitialized variables are undefined', () => {
+    // act
+    let aNumber;
+    // assert
+    assert.equal('undefined', typeof aNumber);
+    assert.equal(undefined, aNumber);
+  });
+  
 });
+describe('constants and variables', () =>{
+  it.skip('const means constant', () => {
+    const aNumber = 1;
+    aNumber = 2;
+  });
+  it('you can change a let variable', () => {
+    // arrange
+    let aNumber = 1;
+
+    // act
+    aNumber = 2;
+
+    // assert
+    assert.equal(aNumber, 2);
+  });
+  it('uninitialized variable are undefined', () =>{
+    // act
+    let aNumber;
+
+    // assert
+    assert.equal('undefined', typeof aNumber);
+    assert.equal(undefined, aNumber);
+  });
+  it('null should be used for not set', () => {
+    // arrange
+    // declaring a dummy function
+    const peekIntoTheBox = () => { return false;}
+
+    // act
+    let isCatAlive = null;
+
+    // assert
+    assert.equal(null, isCatAlive);
+    isCatAlive = peekIntoTheBox();
+  });
+})
